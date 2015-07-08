4944fd6f5c5be496b90c3200007d706d44dfe210
laminelli <extralinedesign@gmail.com>
2015-07-08 22:48:02 +0200

    Unfiltered actions.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   respond_to :html, only: 'none'
 
   before_action :guest_or_current, only: :create
+  skip_before_action :verify_authenticity_token
 
   def guest_or_current
     if current_user && current_user.guest?
