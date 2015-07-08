af41314553867a49ad980a8a2f96b5f67d379500
laminelli <extralinedesign@gmail.com>
2015-07-08 19:46:49 +0200

    Formatter.

--- app/serializers/upload_serializer.rb
+++ app/serializers/upload_serializer.rb
@@ -15,10 +15,18 @@ class UploadSerializer < ActiveModel::Serializer
   def title
     if object.location_id.present?
       "#{description}
-      <div class='location'>#{object.location.lat}, #{object.location.lng}</div>"
+      <div class='location'>#{location_time(object.location.lat,'N')}, #{location_time(object.location.lng, 'E')}</div>"
     else
       description
     end
+  end
+
+  def location_time(deg, pos = 'N')
+    d  = deg.to_i
+    md = (deg - d).abs * 60
+    m  = md.to_i
+    sd = (md - m) * 60
+    "#{d}°#{m}'#{sd}\"#{pos}"
   end
 
   def src
